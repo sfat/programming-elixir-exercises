969395b0061a76079a7e01cd1bf233159e285175
sfat <andrei@sfat.me>
2019-02-10 15:49:41 +0200

    added final version of issues project

--- organizing-a-project/issues/lib/issues/cli.ex
+++ organizing-a-project/issues/lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2]
   @default_count 4
 
   @moduledoc """
@@ -6,9 +8,26 @@ defmodule Issues.CLI do
   the various functions that end up generating a
   table of the last _n_ issues in a github project
   """
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
 
-  def run(argv) do
-    parse_args(argv)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
   end
 
   @doc """
@@ -20,17 +39,44 @@ defmodule Issues.CLI do
   Return a tuple of `{ user, project, count}`, or `:help` if help was given.
   """
   def parse_args(argv) do
-    parse =
-      OptionParser.parse(argv,
-        switches: [help: :boolean],
-        aliases: [h: :help]
-      )
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
-      {_, [user, project], _} -> {user, project, @default_count}
-      _ -> :help
-    end
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  # bad arg or --help
+  def args_to_internal_representation(_) do
+    :help
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
   end
 end
--- organizing-a-project/issues/mix.exs
+++ organizing-a-project/issues/mix.exs
@@ -4,7 +4,10 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
-      version: "0.1.0",
+      escript: escript_config(),
+      version: "0.0.1",
+      name: "Issues",
+      source_url: "https://github.com/sfat/programming-elixir/issues",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,8 +24,16 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.0.0"},
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.19.3"},
+      {:earmark, "~> 1.2.4"}
+    ]
+  end
+  
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
