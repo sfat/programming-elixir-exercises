f31e8581e4f03b254d0f79ce258e1c41fef2f0f7
sfat <andrei@sfat.me>
2019-02-19 22:45:55 +0200

    added exercises from OTP: Applications chapter - second version

--- otp-applications/sequence/lib/sequence/server.ex
+++ otp-applications/sequence/lib/sequence/server.ex
@@ -1,7 +1,12 @@
 defmodule Sequence.Server do
   use GenServer
+  require Logger
   
-  @vsn "0"
+  @vsn "1"
+  
+  defmodule State do
+    defstruct(current_number: 0, delta: 1)
+  end
   
   #######
   # External API
@@ -24,22 +29,30 @@ defmodule Sequence.Server do
   # GenServer implementation
   
   def init(_) do
-    {:ok, Sequence.Stash.get()}
+    state = %State{current_number: Sequence.Stash.get()}
+    {:ok, state}
   end
   
-  def handle_call(:next_number, _from, current_number) do
-    {:reply, current_number, current_number + 1}
+  def handle_call(:next_number, _from, state = %{current_number: n}) do
+    {:reply, n, %{state | current_number: n + state.delta}}
   end
   
-  def handle_call({:set_number, new_number}, _from, _current_number) do
-    {:reply, new_number, new_number}
-  end
-  
-  def handle_cast({:increment_number, delta}, current_number) do
-    {:noreply, current_number + delta}
+  def handle_cast({:increment_number, delta}, state) do
+    {:noreply, %{state | delta: delta}}
   end
   
   def terminate(_reason, current_number) do
     Sequence.Stash.update(current_number)
+  end
+  
+  def code_change("0", old_state = current_number, _extra) do
+    new_state = %State{
+      current_number: current_number,
+      delta: 1
+    }
+    Logger.info "Changing code from 0 to 1"
+    Logger.info inspect(old_state)
+    Logger.info(inspect(new_state))
+    {:ok, new_state}
   end
 end
